822d62c5901bc07c19f4aa20167eb547c8238742
asararatnakar <asara.ratnakar@gmail.com>
2018-06-26 23:35:12 -0400

    first commit

---
+++ network_setup.sh
@@ -0,0 +1,170 @@
+#!/bin/bash
+#
+# Copyright IBM Corp. All Rights Reserved.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+
+UP_DOWN="$1"
+CH_NAME="$2"
+CLI_TIMEOUT="$3"
+IF_COUCHDB="$4"
+
+: ${CLI_TIMEOUT:="10000"}
+
+COMPOSE_FILE=docker-compose-cli.yaml
+COMPOSE_FILE_COUCH=docker-compose-couch.yaml
+export PATH=${PWD}/../bin:${PWD}:$PATH
+export FABRIC_CFG_PATH=${PWD}
+
+if [ ! -d bin ]; then
+	curl -sSL http://bit.ly/2ysbOFE | bash -s 1.2.0-rc1
+fi
+
+function printHelp () {
+	echo "Usage: ./network_setup <up|down> <\$channel-name> <\$cli_timeout> <couchdb>.\nThe arguments must be in order."
+}
+
+function validateArgs () {
+	if [ -z "${UP_DOWN}" ]; then
+		echo "Option up / down / restart not mentioned"
+		printHelp
+		exit 1
+	fi
+	if [ -z "${CH_NAME}" ]; then
+		echo "setting to default channel 'mychannel'"
+		CH_NAME=mychannel
+	fi
+}
+
+function clearContainers () {
+        CONTAINER_IDS=$(docker ps -aq)
+        if [ -z "$CONTAINER_IDS" -o "$CONTAINER_IDS" = " " ]; then
+                echo "---- No containers available for deletion ----"
+        else
+                docker rm -f $CONTAINER_IDS
+        fi
+}
+
+function removeUnwantedImages() {
+        DOCKER_IMAGE_IDS=$(docker images | grep "dev\|none\|test-vp\|peer[0-9]-" | awk '{print $3}')
+        if [ -z "$DOCKER_IMAGE_IDS" -o "$DOCKER_IMAGE_IDS" = " " ]; then
+                echo "---- No images available for deletion ----"
+        else
+                docker rmi -f $DOCKER_IMAGE_IDS
+        fi
+}
+
+## Generates Org certs using cryptogen tool
+function generateCerts (){
+	echo "##########################################################"
+	echo "##### Generate certificates using cryptogen tool #########"
+	echo "##########################################################"
+	cryptogen generate --config=./crypto-config.yaml
+	echo
+}
+
+function generateIdemixMaterial (){
+	CURDIR=`pwd`
+	IDEMIXMATDIR=$CURDIR/crypto-config/idemix
+
+	echo
+	echo "####################################################################"
+	echo "##### Generate idemix crypto material using idemixgen tool #########"
+	echo "####################################################################"
+
+	mkdir -p $IDEMIXMATDIR
+	cd $IDEMIXMATDIR
+
+	# Generate the idemix issuer keys
+	idemixgen ca-keygen
+
+	# Generate the idemix signer keys
+	idemixgen signerconfig -u OU1 -e OU1 -r 1
+
+	cd $CURDIR
+}
+
+## Generate orderer genesis block , channel configuration transaction and anchor peer update transactions
+function generateChannelArtifacts() {
+
+	CONFIGTXGEN=configtxgen
+
+	echo "##########################################################"
+	echo "#########  Generating Orderer Genesis block ##############"
+	echo "##########################################################"
+	# Note: For some unknown reason (at least for now) the block file can't be
+	# named orderer.genesis.block or the orderer will fail to launch!
+	$CONFIGTXGEN -profile TwoOrgsOrdererGenesis -channelID e2e-orderer-syschan -outputBlock ./channel-artifacts/genesis.block
+
+	echo
+	echo "#################################################################"
+	echo "### Generating channel configuration transaction 'channel.tx' ###"
+	echo "#################################################################"
+	$CONFIGTXGEN -profile TwoOrgsChannel -outputCreateChannelTx ./channel-artifacts/channel.tx -channelID $CHANNEL_NAME
+
+	echo
+	echo "#################################################################"
+	echo "#######    Generating anchor peer update for Org1MSP   ##########"
+	echo "#################################################################"
+	$CONFIGTXGEN -profile TwoOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/Org1MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org1MSP
+
+	echo
+	echo "#################################################################"
+	echo "#######    Generating anchor peer update for Org2MSP   ##########"
+	echo "#################################################################"
+	$CONFIGTXGEN -profile TwoOrgsChannel -outputAnchorPeersUpdate ./channel-artifacts/Org2MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org2MSP
+	echo
+}
+
+function networkUp () {
+    if [ -d "./crypto-config" ]; then
+      echo "crypto-config directory already exists."
+    else
+      #Generate all the artifacts that includes org certs, orderer genesis block,
+      # channel configuration transaction
+      generateCerts
+      generateIdemixMaterial
+      generateChannelArtifacts
+    fi
+
+    if [ "${IF_COUCHDB}" == "couchdb" ]; then
+      CHANNEL_NAME=$CH_NAME TIMEOUT=$CLI_TIMEOUT docker-compose -f $COMPOSE_FILE -f $COMPOSE_FILE_COUCH up -d 2>&1
+    else
+      CHANNEL_NAME=$CH_NAME TIMEOUT=$CLI_TIMEOUT docker-compose -f $COMPOSE_FILE up -d 2>&1
+    fi
+    if [ $? -ne 0 ]; then
+	echo "ERROR !!!! Unable to pull the images "
+	exit 1
+    fi
+    docker logs -f cli
+}
+
+function networkDown () {
+    docker-compose -f $COMPOSE_FILE -f $COMPOSE_FILE_COUCH down
+
+    #Cleanup the chaincode containers
+    clearContainers
+
+    #Cleanup images
+    removeUnwantedImages
+
+    # remove orderer block and other channel configuration transactions and certs
+    rm -rf channel-artifacts/*.block channel-artifacts/*.tx crypto-config
+}
+
+validateArgs
+
+#Create the network using docker compose
+if [ "${UP_DOWN}" == "up" ]; then
+	networkUp
+elif [ "${UP_DOWN}" == "down" ]; then ## Clear the network
+	networkDown
+elif [ "${UP_DOWN}" == "restart" ]; then ## Restart the network
+	networkDown
+	networkUp
+else
+	printHelp
+	exit 1
+fi
